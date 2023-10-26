a06f5073c54b1f1acd2508272a16dd1b7c0bee63
FiFi <148422677+Adufifi@users.noreply.github.com>
2023-10-26 12:35:42 +0700

    Add files via upload

---
+++ Shape.java
@@ -0,0 +1,15 @@
+package com.mycompany.p0061;
+
+public abstract class Shape {
+
+    public double getPerimeter() {
+        return 0;
+    }
+
+    public double getArea() {
+        return 0;
+    }
+
+    public void printResult() {
+    }
+}
